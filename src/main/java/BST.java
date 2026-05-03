6719d2e8c37a5c1c0c8b5798207193ad989f2ca5
Asus <sseka2512@gmail.com>
2026-05-03 19:57:04 +0500

    implement basic Binary Search Tree (put and get)

--- src/main/java/BST.java
+++ src/main/java/BST.java
@@ -1,2 +1,45 @@
 public class BST<K extends Comparable<K>, V> {
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    private Node root;
+
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
+
+    private Node put(Node x, K key, V val) {
+        if (x == null) return new Node(key, val);
+
+        int cmp = key.compareTo(x.key);
+
+        if (cmp < 0) x.left = put(x.left, key, val);
+        else if (cmp > 0) x.right = put(x.right, key, val);
+        else x.val = val;
+
+        return x;
+    }
+
+    public V get(K key) {
+        Node x = root;
+
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+
+            if (cmp < 0) x = x.left;
+            else if (cmp > 0) x = x.right;
+            else return x.val;
+        }
+
+        return null;
+    }
 }
